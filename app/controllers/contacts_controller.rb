18f4fa7ddfe104a37af37e9a3ee3ac072d965f2b
Remmide Sid <kelmipappi.email@gmail.com>
2017-03-17 14:52:29 +0000

    Added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,17 @@
 class ContactsController < ApplicationController
     def new
+        @contact= Contact.new
+    end
+    def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+           redirect_to new_contact_path,notice: "Message envoyé." 
+        else
+             redirect_to new_contact_path,notice: "Erreur."
+        end    
+    end
+    private 
+    def contact_params
+        params.require(:contact).permit(:nom, :email, :commentaires)
     end
 end
